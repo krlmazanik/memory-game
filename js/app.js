5ba05253c69d9d97c2583d8a76252ab2cc30ebe8
Kiryl Mazanik <krlmazanik@gmail.com>
2017-10-17 20:28:25 +0200

    removed jquery as dependancy

--- js/app.js
+++ js/app.js
@@ -1,38 +1,69 @@
-$(document).ready(function(){
-	let blocks = 16;
+var colorPick = [
+	'orange', 
+	'yellow', 
+	'gray', 
+	'brown', 
+	'blue', 
+	'green', 
+	'red', 
+	'purple'
+];
+colorPick.forEach(function(color){colorPick.push(color)});
+var chosenDiv = document.querySelectorAll('.closed');
+var openedCells;
 
-	$('.closed').click(function(){
-		$(this).removeClass('closed');
-		$(this).addClass('open');
+function shuffleColors(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
-		let length = $('.open').length;
-		let open1 = $('.open')[0];
-		let open2 = $('.open')[1];
-		let open1color = $(open1).css('backgroundColor');
-		let open2color = $(open2).css('backgroundColor');
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
 
-		if (length == 2) {
-			if(open1color === open2color) {
-				setTimeout(function(){
-					$('.open').addClass('done');
-					$('.open').removeClass('open');
-					blocks -= 2;
-					document.getElementById('left').innerHTML = blocks;
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
 
-					if($('.done').length > 14) {
-						$('.open').addClass('done');
-						$('.open').removeClass('open');
-						alert('You have finished the game! Congratulations!');
-						window.history.go(0);
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function setUpCells() {
+	shuffleColors(colorPick);
+	for (var i = 0; i < chosenDiv.length; i++) {
+		chosenDiv[i].style.backgroundColor = colorPick[i];
+		chosenDiv[i].setAttribute('col-atr', colorPick[i]); 
+		chosenDiv[i].addEventListener("click", setUpLogic); 
+	}
+}
+
+function setUpLogic() {
+	if (this.classList.contains('closed')) {
+		this.classList.remove("closed");
+		this.classList.add("opened");
+		openedCells = document.querySelectorAll('.opened');
+		if (openedCells.length === 2) {
+			let firstEl = openedCells[0].getAttribute('col-atr');
+			let secondEl = openedCells[1].getAttribute('col-atr');
+			if (firstEl === secondEl) {
+				for (let i = 0; i < openedCells.length; i++) {
+					openedCells[i].removeEventListener('click', false);
+					openedCells[i].style.backgroundColor = '';
+					openedCells[i].classList.remove('opened');
+					} 
+				} else {
+						for (let i = 0; i < openedCells.length; i++){
+							setTimeout(function() {
+								openedCells[i].classList.add('closed');
+								openedCells[i].classList.remove('opened');
+							}, 500);
+						}
 					}
-
-				}, 300);
-			} else {
-				setTimeout(function(){
-					$('.open').addClass('closed');
-					$('.open').removeClass('open');
-				}, 300);
-			}
 		}
-	});
-});
+	}
+};
+
+setUpCells();
