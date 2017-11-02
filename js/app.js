389586f51ccc57765bdc4b28c4935b97d7a7ec4c
Kiryl Mazanik <krlmazanik@gmail.com>
2017-11-02 14:43:18 +0100

    a bit of refactoring

--- js/app.js
+++ js/app.js
@@ -8,26 +8,22 @@ var colorPick = [
 	'red', 
 	'purple'
 ];
-colorPick.forEach(function(color){colorPick.push(color)});
 var chosenDiv = document.querySelectorAll('.closed');
 var openedCells;
+
+colorPick.forEach(function(color){colorPick.push(color)});
 
 function shuffleColors(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
-  // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-
-    // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
 
-    // And swap it with the current element.
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
   }
-
   return array;
 }
 
